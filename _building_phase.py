649fa4f451c2bda39ba2862a4e0d107c39ca9a34
Em3raud3 <animekamisama@gmail.com>
2021-07-31 13:50:45 -0400

    f

--- _building_phase.py
+++ _building_phase.py
@@ -1,9 +1,24 @@
-s = "babad"
+str = "abba"
 
 
-def search_from_middle(str, left, right):
-    if not str or left > right:
-        print(0)
-        exit()
+def expand_from_middle(s, left, right):
 
-    while left
+    if not s or left > right:
+        return 0
+
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+
+    return(right - left - 1)
+
+
+mStr = ""
+
+for i in range(len(str)):
+    l1 = expand_from_middle(str, i, i)
+    l2 = expand_from_middle(str, i, i + 1)
+    l = max(l1, l2)
+
+
+print(maxL)
