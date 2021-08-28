daf6161ffe66578defcb2409af196983939f721f
Emeraude <animekamisama@gmail.com>
2021-08-28 11:45:14 -0400

    updated

---
+++ 380_insert_delete_getrandom_(O1).py
@@ -0,0 +1,50 @@
+import random
+class RandomizedSet(object):
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.rset = set()
+        self.rarr = list()
+        
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        self.val = val
+        if val in self.rset:
+            return False
+        
+        else:
+            self.rset.add(val)
+            self.rarr.append(val)
+            return  True
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        self.val = val
+        
+        if val in self.rset:
+            self.rset.remove(val)
+            self.rarr.remove(val)
+            return  True
+        
+        else: 
+            return False
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        target = self.rarr[random.randint(0,len(self.rarr) - 1)]
+        return target
