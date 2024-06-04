9171901f49be05c1aaf018c20c4bff9a9dbdc503
Em3raud3 <animekamisama@gmail.com>
2024-06-04 13:04:47 -0400

    12/4/24

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,6 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
+
+        for index, i in enumerate(nums):
+
+            result = []
+            result.append(index)
+
+            pair = target - i
+
+            if pair in nums:
+
+                if pair == i and nums.count(i) == 1:
+                    continue
+
+                elif pair == i and nums.count(i) > 1:
+                    result.append(nums.index(pair, index + 1))
+                    return result
+
+                else:
+                    result.append(nums.index(pair))
+                    return result
